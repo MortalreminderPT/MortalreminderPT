fdcd3adcc484048ed35debaa28300caad781f24c
PT <Mortalreminder.PT@outlook.com>
2023-06-12 22:07:56 +0800

    New post for today

---
+++ upload/upload.sh
@@ -0,0 +1,6 @@
+jekyll build
+cd D:\Github\MortalreminderPT.github.io\_site\
+cd D:\Github\MortalreminderPT.github.io\_site\
+cd D:\Github\MortalreminderPT.github.io\_site\
+git commit -m "New post for today"
+git push
