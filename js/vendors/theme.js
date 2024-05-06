7b4a77ef0c1816a913fb7a5925f9943bbf861f70
mortalreminder <me@mortalreminderpt.net>
2024-05-06 19:05:03 +1000

    Fix bugs

--- js/vendors/theme.js
+++ js/vendors/theme.js
@@ -26,6 +26,24 @@ const nightStyles = {
     "--middle-grey-lightness": "100%"
 }
 
+function isNight() {
+    const theme = sessionStorage.getItem("theme");
+    var night = false;
+    if (theme === 'night') {
+        night = true;
+    } else if (theme === 'day') {
+        night = false;
+    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        night = true;
+    } else {
+        var now = new Date();
+        var hour = now.getHours();
+        night = hour < 6 || hour >= 18;
+    }
+    console.log("night = " + night);
+    return night;
+};
+
 document.addEventListener("DOMContentLoaded", function() {
     // 检测浏览器是否支持prefers-color-scheme媒体特性
     if (isNight()) {
@@ -41,30 +59,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-function isNight() {
-    const theme = sessionStorage.getItem("theme");
-    if (theme === 'night') {
-        return true;
-    } 
-    if (theme === 'day') {
-        return false;
-    }
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        return true;
-    } else {
-        var now = new Date();
-        var hour = now.getHours();
-        return hour < 6 || hour >= 18;
-    }
-}
-
 var ThemeSwitch = {
     init: function() {
-      var switchButton = document.getElementById('js-theme-switch');
-      switchButton.addEventListener('click', this.switchTheme);
-      var switchPopButton = document.getElementById('js-theme-switch-pop');
-      switchPopButton.addEventListener('click', this.switchTheme);
+        var switchButton = document.getElementById('js-theme-switch');
+        switchButton.addEventListener('click', this.switchTheme);
+        var switchPopButton = document.getElementById('js-theme-switch-pop');
+        switchPopButton.addEventListener('click', this.switchTheme);
     },
+
     switchTheme: function() {
         if (isNight()) {
             sessionStorage.setItem("theme", "day");
