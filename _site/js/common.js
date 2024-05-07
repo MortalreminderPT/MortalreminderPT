a82fa7aae2118850afea85f7cfeee0636905abc6
mortalreminder <me@mortalreminder.net>
2024-05-07 22:27:03 +1000

    Fix the search and 404 bugs of i18n

--- _site/js/common.js
+++ _site/js/common.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
   SimpleJekyllSearch({
     searchInput: document.getElementById("js-search-input"),
     resultsContainer: document.getElementById("js-results-container"),
-    json: '/search.json',
+    json: site_lang + '/search.json',
     searchResultTemplate: '{article}',
     noResultsText: '<li class="no-results"><h3>No results found</h3></li>'
   });
